4f3e4bd242f31cbe4d4690b914e5ab49927dd98b
Yohannes <ydegu@ADMIN.CROSSROADS.CO>
2023-07-07 13:37:22 +0300

    new exprience

--- src/components/data/data.jsx
+++ src/components/data/data.jsx
@@ -50,6 +50,16 @@ export const project = [
     link: "https://real-state-two.vercel.app/",
   },
   {
+    name: "Real State (Personal Project)",
+    disc: `Web based Real State App, which includes rental homes and for-sale properties, advanced property filtering, property details page. Used React JS, Next js,ChakraUI for User Interface, and Rapid Api`,
+    link: "https://real-state-two.vercel.app/",
+  },
+  {
+    name: "Real State (Personal Project)",
+    disc: `Web based Real State App, which includes rental homes and for-sale properties, advanced property filtering, property details page. Used React JS, Next js,ChakraUI for User Interface, and Rapid Api`,
+    link: "https://real-state-two.vercel.app/",
+  },
+  {
     name: "awakilo",
     disc: `Web based website Used to extention service for coffee farmers...`,
     link: "https://awakilo.com/",
